f0f760561809d191ffd30ebf8957c3341a2b8556
ramyo564 <yohan032yohan@gmail.com>
2023-08-10 20:44:29 +0900

    Build: Login User

--- eshop/urls.py
+++ eshop/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for eshop project.
+"""eshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('product.urls'))
+    path('api/', include('product.urls')),
+    path('api/', include('account.urls')),
+    path('api/token/', TokenObtainPairView.as_view())
 ]
+
 
 handler404 = 'utils.error_views.handler404'
 handler500 = 'utils.error_views.handler500'
